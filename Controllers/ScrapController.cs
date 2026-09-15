89649aaebef18098c584b3c67cc20497c2927ee7
Andrew Harris <aharris36@my.wctc.edu>
2026-09-15 18:49:10 -0500

    detail pages

--- Controllers/ScrapController.cs
+++ Controllers/ScrapController.cs
@@ -10,4 +10,12 @@ public class ScrapController : Controller
     {
         return View(ScrapData.All);
     }
+    public IActionResult Details(int id)
+    {
+        if (ScrapData.All.FirstOrDefault(c => c.Id == id) == null)
+        {
+            return NotFound();
+        }
+        return View(ScrapData.All.FirstOrDefault(c => c.Id == id));
+    }
 }
