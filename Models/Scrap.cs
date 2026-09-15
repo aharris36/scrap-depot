ee322b2d42c799b1f201d502a56475d50189af42
Andrew Harris <aharris36@my.wctc.edu>
2026-09-15 18:03:25 -0500

    scrap model

---
+++ Models/Scrap.cs
@@ -0,0 +1,11 @@
+namespace scrap_depot.Models;
+
+public class Scrap
+{
+    public int Id {get;set;}
+    public String Name {get;set;} = "";
+    public String Description {get;set;} = "";
+    public int Weight {get;set;}
+    public int Depositied {get;set;}
+
+}
